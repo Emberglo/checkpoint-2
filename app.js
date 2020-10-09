861ad9facd8ee278c4877c1b4b418defb503de84
Ryan Carpenter <rccarpenter@outlook.com>
2020-10-09 12:14:16 -0600

    added skeleton, images, and mine logic

---
+++ app.js
@@ -0,0 +1,26 @@
+let plutonium = 0;
+
+
+
+function mine() {
+    plutonium++;
+    console.log(plutonium);
+}
+
+function buyPickaxe() {
+
+}
+
+function buyHovercraft() {
+
+}
+
+function buyPlutonian() {
+
+}
+
+function buyDrill() {
+
+}
+
+mine();
